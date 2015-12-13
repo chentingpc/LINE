a02ed013ec94ffe2d3f0be23662cfdd638d1d582
ting <iamtingchen@gmail.com>
2015-12-13 11:35:14 -0500

    add blogcatalog data & update train.sh

--- linux/train.sh
+++ linux/train.sh
@@ -7,9 +7,18 @@ g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unus
 g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result concatenate.cpp -o concatenate
 
 data_folder=data/
-#data_name=youtube
 data_name=blogcatalog
+#data_name=youtube
 result_folder=result/
+
+if [ ! -e $data_folder/$data_name.txt ]; then
+    echo 'ERROR: data not found.'
+    exit
+fi
+
+if [ ! -e $result_folder ]; then
+    mkdir $result_folder
+fi
 
 ./reconstruct -train $data_folder/$data_name.txt -output $data_folder/$data_name\_dense.txt -depth 2 -k-max 1000
 ./line -train $data_folder/$data_name\_dense.txt -output $result_folder/vec_1st_wo_norm.txt -binary 1 -size 128 -order 1 -negative 5 -samples 10000 -threads 6
